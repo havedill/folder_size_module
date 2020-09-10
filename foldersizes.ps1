1b9c455d65975b17b435398c504b2e4a14cb1eca
havedill <34009509+havedill@users.noreply.github.com>
2020-09-10 07:12:16 -0500

    Update foldersizes.ps1

--- foldersizes.ps1
+++ foldersizes.ps1
@@ -10,15 +10,17 @@ $sizeinfo = @{}
 
 foreach ($i in $subDirectories.fullName)
 	{
-    $size=((robocopy.exe $i c:\fakepathduh /L /XJ /R:0 /W:1 /NP /E /BYTES /NFL /NDL /NJH /MT:64)[-4] -replace '\D+(\d+).*','$1') / 1MB -as [int]
+    $size=((robocopy.exe $i c:\fakepathduh /L /XJ /R:0 /W:1 /NP /E /BYTES /NFL /NDL /NJH /MT:64)[-4] -replace '\D+(\d+).*','$1') -as [int]
     $sizeinfo.Add($i.Replace("\","/"), $size)
    
 }
 
 $topvalues = ($sizeinfo.GetEnumerator() | Sort-Object -Property Value -Descending | select -first $topcount)
-$topvalues | select Name,Value | Export-Csv $csvpath
+#$topvalues | select Name,Value | Export-Csv $csvpath
 foreach($value in $topvalues){
     $data += [PSCustomObject]@{"{#DIR}" = $value.name}
+    C:\zabbix\bin\zabbix_sender.exe -c "C:\zabbix\conf\zabbix_agentd.conf" -s $ENV:computername -k "folder.size[$($value.name)]" -o $value.value
+
 }
 $json = [PSCustomObject]@{"data" = $data}
 $json | convertto-json
