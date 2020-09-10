3f276a441ab49432688becd1fb6893a279fadec1
havedill <34009509+havedill@users.noreply.github.com>
2020-09-10 08:35:04 -0500

    Update foldersizes.ps1

--- foldersizes.ps1
+++ foldersizes.ps1
@@ -11,17 +11,16 @@ foreach ($i in $subDirectories.fullName)
 	{
     $size=((robocopy.exe $i c:\fakepathduh /L /XJ /R:0 /W:1 /NP /E /BYTES /NFL /NDL /NJH /MT:64)[-4] -replace '\D+(\d+).*','$1')
     $sizeinfo.Add($i.Replace("\","/"), $size)
-   
 }
 
 $topvalues = ($sizeinfo.GetEnumerator() | Sort-Object -Property Value -Descending | select -first $topcount)
+#$topvalues | select Name,Value | Export-Csv $csvpath
 foreach($value in $topvalues){
+    $data += [PSCustomObject]@{"{#DIR}" = $value.name}
+ 
     if($value.value -eq ""){
         $value.value = -1000
     }
-    $data += [PSCustomObject]@{"{#DIR}" = $value.name}
-        
-    #remove out-null and add -vv for debugging if your not seeing items populate
     C:\zabbix\bin\zabbix_sender.exe -c "C:\zabbix\conf\zabbix_agentd.conf" -s $ENV:computername -k "folder.size[$($value.name)]" -o $value.value | out-null
 
 }
